94331b6a30f2b8f9b779c8aa72b7fb5755366ff9
Henrique Shiraishi <henriqueashiraishi@gmail.com>
2023-01-10 01:53:34 +0000

    Initial release 	modified:   .gitignore

---
+++ test/test_sefaz.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestSefaz < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Sefaz::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
